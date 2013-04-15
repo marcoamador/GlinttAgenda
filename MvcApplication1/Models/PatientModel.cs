27a12f5c3814b049320ff9d41aae808264201247
highvolt <highvolt16@gmail.com>
2013-04-15 19:45:44 +0100

    Patient almost

--- MvcApplication1/Models/PatientModel.cs
+++ MvcApplication1/Models/PatientModel.cs
@@ -5,12 +5,39 @@ using System.Web;
 
 namespace MvcApplication1.Models
 {
+
+   
     public class PatientModel
     {
+        Hl7.Fhir.Model.Patient p;
         glinttEntities gE;
         public PatientModel() {
             gE = new glinttEntities();
+            p = new Hl7.Fhir.Model.Patient();
 
         }
+    
+        public string byId(string id){
+            g_doente patient= gE.g_doente.Find(id);
+           
+            Hl7.Fhir.Model.Identifier i=new Hl7.Fhir.Model.Identifier();
+            i.Id=patient.doente;
+            i.InternalId=patient.t_doente;
+            
+            p.Identifier.Add(i); //errado
+
+            Hl7.Fhir.Model.Demographics dem = new Hl7.Fhir.Model.Demographics();
+            dem.Identifier.Add(i); //errado
+            dem.InternalId = patient.t_doente;
+            Hl7.Fhir.Model.HumanName human=new Hl7.Fhir.Model.HumanName();
+            human.Text=patient.nome;
+            dem.Name.Add(human)
+
+
+            return Hl7.Fhir.Serializers.FhirSerializer.SerializeResourceAsXml(p);
+        }
+    
     }
+  
+
 }
