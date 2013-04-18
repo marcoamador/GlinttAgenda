16e9738f177b6fbf0551a2b790a2acdf943b6496
Maria Joao <mjoao.b3@gmail.com>
2013-04-18 22:55:14 +0100

    Alteração do ficheiro Visit.cs

--- MvcApplication1/Models/Visit.cs
+++ MvcApplication1/Models/Visit.cs
@@ -2,28 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 
 namespace MvcApplication1.Models
 {
-    public class Visit
+    public class VisitModel
     {
-        Hl7.Fhir.Model.Visit v;
-        glinttEntities gE;
-
-        public Visit()
-        {
-            gE = new glinttEntities();
-            v = new Hl7.Fhir.Model.Visit();
+        public VisitModel()
+        { 
+              ge = new glinttEntities();
         }
 
-        /*public String byId(string id)
-        {
-            Object[] key = { id };
-            g_cons_marc visit = gE.g_cons_marc.SqlQuery("Select * from g_cons_marc where n_cons=?", key).First();
+     glinttEntities ge;
 
-            Hl7.Fhir.Model.Identifier i = new Hl7.Fhir.Model.Identifier();
-            i.Id = visit.n_cons;
-        }*/
+    //STATE, SETTING, 
+
+     public string byID(int id) {
+
+         Hl7.Fhir.Model.Visit v = new Hl7.Fhir.Model.Visit();
+
+         g_cons_marc c = ge.g_cons_marc.Find(id);
+
+         Hl7.Fhir.Model.Identifier idt = new Hl7.Fhir.Model.Identifier();
+         idt.Id = c.n_cons;
+
+         Hl7.Fhir.Model.CodeableConcept state = new Hl7.Fhir.Model.CodeableConcept();
+         state.Text = c.flag_estado; //TODO errado
+
+         Hl7.Fhir.Model.ResourceReference rr = new Hl7.Fhir.Model.ResourceReference();
+         rr.InternalId = c.doente;
+
+         v.Identifier = new List<Hl7.Fhir.Model.Identifier>() {idt};
+         v.State = new Hl7.Fhir.Model.CodeableConcept();
+         v.Setting = new Hl7.Fhir.Model.CodeableConcept();
+         v.Subject = new Hl7.Fhir.Model.ResourceReference();
+     }
+     
+
+  
+ 
     }
+
+
 }
