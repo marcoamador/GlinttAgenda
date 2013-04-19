fcc7c9aa3eebd6f329a8faaec8817f0b9a827036
Daniel Freitas <danielfreitas9@gmail.com>
2013-04-19 03:08:19 +0100

    Método para retornar consultas de médico ou paciente (Inacabado)

--- MvcApplication1/Models/Visit.cs
+++ MvcApplication1/Models/Visit.cs
@@ -25,70 +25,93 @@ namespace MvcApplication1.Models
         };
 
         public VisitModel()
-        { 
-              ge = new glinttEntities();
+        {
+            ge = new glinttEntities();
         }
 
-     glinttEntities ge;
+        glinttEntities ge;
 
-    //RESOURCE REFERENCES->SUBJECT, RESPONSIBLE, FULFILLS, CONTACT, INDICATION 
+        //RESOURCE REFERENCES->SUBJECT, RESPONSIBLE, FULFILLS, CONTACT, INDICATION 
 
-     public string visitParser(g_cons_marc c) {
+        public string visitParser(g_cons_marc c)
+        {
 
-         Hl7.Fhir.Model.Visit v = new Hl7.Fhir.Model.Visit();
-
-
-         Hl7.Fhir.Model.Identifier idt = new Hl7.Fhir.Model.Identifier();
-         idt.Id = c.n_cons;
-
-         Hl7.Fhir.Model.CodeableConcept state = new Hl7.Fhir.Model.CodeableConcept();
-         state.Text = c.flag_estado; //TODO errado
-
-         Hl7.Fhir.Model.CodeableConcept set = new Hl7.Fhir.Model.CodeableConcept();
-         set.Text = null;
+            Hl7.Fhir.Model.Visit v = new Hl7.Fhir.Model.Visit();
 
 
-        //TODO faltam todos os resource references
-         Hl7.Fhir.Model.ResourceReference rr = new Hl7.Fhir.Model.ResourceReference();
-         
-         Hl7.Fhir.Model.Period periodo = new Hl7.Fhir.Model.Period();
-         //periodo.Start = c.dt_cons;
-         //periodo.End = 
+            Hl7.Fhir.Model.Identifier idt = new Hl7.Fhir.Model.Identifier();
+            idt.Id = c.n_cons;
 
-         Hl7.Fhir.Model.Duration duracao = new Hl7.Fhir.Model.Duration();
-         //DURACAO = PERIODO
+            Hl7.Fhir.Model.CodeableConcept state = new Hl7.Fhir.Model.CodeableConcept();
+            state.Text = c.flag_estado; //TODO errado
+
+            Hl7.Fhir.Model.CodeableConcept set = new Hl7.Fhir.Model.CodeableConcept();
+            set.Text = null;
 
 
-         v.Identifier = new List<Hl7.Fhir.Model.Identifier>() {idt};
-         v.State = state;
-         v.Setting = new Hl7.Fhir.Model.CodeableConcept();
-         v.Subject = new Hl7.Fhir.Model.ResourceReference();
-         v.Responsible = new Hl7.Fhir.Model.ResourceReference();
-         v.Fulfills = new Hl7.Fhir.Model.ResourceReference();
-         v.Period = periodo;
-         v.Length = duracao;
-         v.Contact = new Hl7.Fhir.Model.ResourceReference();
-         v.Indication = new Hl7.Fhir.Model.ResourceReference();
+            //TODO faltam todos os resource references
+            Hl7.Fhir.Model.ResourceReference rr = new Hl7.Fhir.Model.ResourceReference();
 
-         return Hl7.Fhir.Serializers.FhirSerializer.SerializeResourceAsXml(v);
-     }
+            Hl7.Fhir.Model.Period periodo = new Hl7.Fhir.Model.Period();
+            //periodo.Start = c.dt_cons;
+            //periodo.End = 
 
-     public String byId(string id)
+            Hl7.Fhir.Model.Duration duracao = new Hl7.Fhir.Model.Duration();
+            //DURACAO = PERIODO
+
+
+            v.Identifier = new List<Hl7.Fhir.Model.Identifier>() { idt };
+            v.State = state;
+            v.Setting = new Hl7.Fhir.Model.CodeableConcept();
+            v.Subject = new Hl7.Fhir.Model.ResourceReference();
+            v.Responsible = new Hl7.Fhir.Model.ResourceReference();
+            v.Fulfills = new Hl7.Fhir.Model.ResourceReference();
+            v.Period = periodo;
+            v.Length = duracao;
+            v.Contact = new Hl7.Fhir.Model.ResourceReference();
+            v.Indication = new Hl7.Fhir.Model.ResourceReference();
+
+            return Hl7.Fhir.Serializers.FhirSerializer.SerializeResourceAsXml(v);
+        }
+
+        public String byId(string id)
+        {
+            Object[] key = { id };
+            System.Data.Entity.Infrastructure.DbSqlQuery<g_cons_marc> sqlresult = ge.g_cons_marc.SqlQuery("Select * from g_cons_marc where n_cons=?", key);
+            if (sqlresult.Count() == 0)
+            {
+                return null;
+            }
+            g_cons_marc visit = sqlresult.First();
+            return visitParser(visit);
+        }
+
+        /*
+     public List<VisitModel> byPatient (string id) 
      {
+
+         List<VisitModel> result = new List<VisitModel>();
          Object[] key = { id };
-         System.Data.Entity.Infrastructure.DbSqlQuery<g_cons_marc> sqlresult = ge.g_cons_marc.SqlQuery("Select * from g_cons_marc where n_cons=?", key);
+         System.Data.Entity.Infrastructure.DbSqlQuery<g_cons_marc> sqlresult = ge.g_cons_marc.SqlQuery("Select * from g_cons_marc where doente=?", key);
          if (sqlresult.Count() == 0)
          {
              return null;
          }
-         g_cons_marc visit = sqlresult.First();
-         return visitParser(visit);
-     }
-     
+    
+         for(int i=0; sqlresult.Count()<i; i++)
+         {
+             g_cons_marc temp = sqlresult.ElementAt(i);
 
+
+             VisitModel final = visitParser(temp);
+             result.Add(temp);
+         }
+         return ;
   
- 
-    }
 
+    }
+        
+ */
+    }
 
 }
